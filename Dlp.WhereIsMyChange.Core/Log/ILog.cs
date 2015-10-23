966f27a1bd3dd0c8f872359a8f89d0f059db90aa
André Galdino <agaldino@mundipagg.com>
2015-10-23 18:32:46 -0200

    Adicionando arquivos faltantes

---
+++ Dlp.WhereIsMyChange.Core/Log/ILog.cs
@@ -0,0 +1,14 @@
+﻿using Dlp.WhereIsMyChange.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dlp.WhereIsMyChange.Core.Log {
+
+    public interface ILog {
+
+        void Save(object logData, LogTypeEnum logType);
+    }
+}
